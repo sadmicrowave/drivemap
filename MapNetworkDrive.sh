2eb219a8360652691e304fd7b41489f11be75e1b
Corey Farmer <corey.farmer@emerus.com>
2016-02-05 11:12:17 -0600

    initial commit

---
+++ MapNetworkDrive.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#############################################################################################################################
+#
+# $$$$$$$\  $$$$$$$\  $$$$$$\ $$\    $$\ $$$$$$$$\       $$\      $$\  $$$$$$\  $$$$$$$\  $$$$$$$\  $$$$$$$$\ $$$$$$$\  
+# $$  __$$\ $$  __$$\ \_$$  _|$$ |   $$ |$$  _____|      $$$\    $$$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ 
+# $$ |  $$ |$$ |  $$ |  $$ |  $$ |   $$ |$$ |            $$$$\  $$$$ |$$ /  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
+# $$ |  $$ |$$$$$$$  |  $$ |  \$$\  $$  |$$$$$\          $$\$$\$$ $$ |$$$$$$$$ |$$$$$$$  |$$$$$$$  |$$$$$\    $$$$$$$  |
+# $$ |  $$ |$$  __$$<   $$ |   \$$\$$  / $$  __|         $$ \$$$  $$ |$$  __$$ |$$  ____/ $$  ____/ $$  __|   $$  __$$< 
+# $$ |  $$ |$$ |  $$ |  $$ |    \$$$  /  $$ |            $$ |\$  /$$ |$$ |  $$ |$$ |      $$ |      $$ |      $$ |  $$ |
+# $$$$$$$  |$$ |  $$ |$$$$$$\    \$  /   $$$$$$$$\       $$ | \_/ $$ |$$ |  $$ |$$ |      $$ |      $$$$$$$$\ $$ |  $$ |
+# \_______/ \__|  \__|\______|    \_/    \________|      \__|     \__|\__|  \__|\__|      \__|      \________|\__|  \__|
+#
+#############################################################################################################################
+
+# Map appropriately specified network drives when exectued
+
+# Define the file to read for the list of servers to attempt to mount
+INPUT_FILE="MOUNTLIST"
+
+# Function to get the users password from the keychain access app.  This way we don't hardcode passwords into the bash script
+get_pw () {
+  security 2>&1 >/dev/null find-internet-password -ga $USERNAME | ruby -e 'print $1 if STDIN.gets =~ /^password: "(.*)"$/'
+}
+
+# Iterate over lines in input file, ignore blank lines with "grep -v "^$"
+for l in $(cat "$INPUT_FILE" | grep -v "^$"); do
+	# Set string splitting delimiter
+	IFS=';'
+	set $l
+
+	SERVER_PATH=${1}
+	SERVER_NAME=${SERVER_PATH%%/*}
+	USERNAME=${2}
+	PW=$(get_pw)
+
+	# Check if password was found for username
+	if [ -z "$PW" ]; then
+		# Skip to next item in loop if password variable is not set
+		continue
+	
+	##########################################################################
+	# TEST IF NETWORK SERVER IF PINGABLE #
+	##########################################################################
+	x=1
+	attempts=5
+	sleep_time=1
+	# Start while loop to test ping status of server
+	while ! ping -c 1 ${SERVER_NAME} &> /dev/null && [ $x -le $attempts ]; do
+		# only enters while loop if server is not pingable
+		# sleep 1 second
+		sleep $sleep_time
+		
+		# increase x counter to get to loop exit threshold
+		x=$(( $x + 1 ))
+	done
+	# test if x = maximum number of attempts and loop was broken because it was reached
+	if [ $x = $attempts ]; then
+		# Skip to next item in loop if server is not pingable
+		continue
+	fi
+	
+		
+	# Ensure the server path derived from INPUT_FILE line item is not already mounted
+	if ! mount | grep "${SERVER_PATH}" > /dev/null; then
+		# Set where we want the mount to occur
+		MOUNT_POINT="/Volumes/"$(basename "${SERVER_PATH}")
+		
+		# Make the base directory for the mount point if necessary
+		if [ ! -d "${MOUNT_POINT}" ]; then
+			mkdir $MOUNT_POINT
+		fi
+		# Perform the mount with type smbfs
+		mount -t smbfs "smb://$USERNAME:$PW@${SERVER_PATH}" "${MOUNT_POINT}"		
+	fi
+done
---
+++ README.md
@@ -0,0 +1,44 @@
+# Drive Map, Bash Automatic Drive Mapping Tool
+Drive Map is an automatic drive mapping tool which maps local
+network servers when the computer initially logs in, wakes, and sleeps.
+The tool utilizes the keychain access app in Mac OS X to retrieve 
+generic internet passwords associated with usernames tied to server paths
+you wish to mount.  
+
+Drive Map is maintained by Emerus, Holdings and is distributed under GNU GPL 3.
+
+# Common Usage
+List the servers and usernames you wish to mount
+in the supplied "MOUNTLIST" file in the following format: 
+<SERVERNAME>;<USERNAME>.  Ensure, the username is associated with a
+password in the keychain access application as an internet password.
+
+## Source content
+
+Here you can find a description of files and directory distributed
+with Drive Map:
+
+* AUTHORS   : development team of Drive Map
+* ChangeLog : log of changes
+* LICENSE   : GNU GPL3 details
+* MapNetworkDrive.sh    : source in Bash
+
+## Licence
+
+Copyright (C) 2015 Emerus, Holdings
+
+This file is part of Drive Map.
+
+Drive Map is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Drive Map is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Drive Map.  If not, see <http://www.gnu.org/licenses/>.
+
